63c4a70eb90b583e1071860d8f1ae52dc5119418
Theo Johnson <txxjohnson@gmail.com>
2022-03-09 22:49:47 -0500

    v2 tests

--- v2/main.js
+++ v2/main.js
@@ -4,8 +4,12 @@ function GetScene (name) {
 	fetch (`scenes/${name}.html`)
 		.then (r => r.text())
 		.then (t => {
+			let parser = new DOMParser();
+			let frag = parser.parseFromString (t, 'text/html');
+			console.log(frag);
 			let v = document.getElementById ('view');
-			v.innerHTML = t;
+//			v.innerHTML = t;
+			v.appendChild (frag);
 			currentScene = name;
 		});
 }
